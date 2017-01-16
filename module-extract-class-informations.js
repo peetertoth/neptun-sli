83213bb45ba6b13159634aca086a27144fc5f3b2
peetertoth <peeter.toth@gmail.com>
2017-01-16 14:24:18 +0100

    update variables

--- module-extract-class-informations.js
+++ module-extract-class-informations.js
@@ -2,30 +2,30 @@ function KeyPress(e) {
       var evtobj = window.event? event : e;
       // Ctrl+Z
       if (evtobj.keyCode == 90 && evtobj.ctrlKey) {
-          titleElement = document.getElementById('upFunction_h_addsubjects_upModal_upmodal_subjectdata_ctl02_Subject_data_for_schedule_upParent_tab_ctl00_upAddSubjects');
-          titleH2 = titleElement.getElementsByTagName('h2')[0];
-          title = titleH2.innerHTML.split('<br>')[0];
-          title = title.split(' (')[0];
+          var titleElement = document.getElementById('upFunction_h_addsubjects_upModal_upmodal_subjectdata_ctl02_Subject_data_for_schedule_upParent_tab_ctl00_upAddSubjects');
+          var titleH2 = titleElement.getElementsByTagName('h2')[0];
+          var title = titleH2.innerHTML.split('<br>')[0];
+          var title = title.split(' (')[0];
 
-          table = document.getElementById('Addsubject_course1_gridCourses_bodytable');
-          tbody = table.getElementsByClassName('scrollablebody')[0];
+          var table = document.getElementById('Addsubject_course1_gridCourses_bodytable');
+          var tbody = table.getElementsByClassName('scrollablebody')[0];
 
-          strToCopy = "";
+          var strToCopy = "";
           for (i = 0; i < tbody.children.length; i++) {
-              curr = tbody.children[i];
-              course = curr.children[1].children[0].innerHTML;
-              courseType = curr.children[2].innerHTML == "Gyakorlat" ? "gy" : "ea";
+              var curr = tbody.children[i];
+              var course = curr.children[1].children[0].innerHTML;
+              var courseType = curr.children[2].innerHTML == "Gyakorlat" ? "gy" : "ea";
               //
-              moreInfo = curr.children[6].innerHTML;
-              day = moreInfo.split(":")[0];
+              var moreInfo = curr.children[6].innerHTML;
+              var day = moreInfo.split(":")[0];
               //
-              indexOfFirstColon = moreInfo.indexOf(':');
-              indexOfFirstWhitespace = moreInfo.indexOf(' ');
-              time = moreInfo.substring(indexOfFirstColon+1, indexOfFirstWhitespace);
+              var indexOfFirstColon = moreInfo.indexOf(':');
+              var indexOfFirstWhitespace = moreInfo.indexOf(' ');
+              var time = moreInfo.substring(indexOfFirstColon+1, indexOfFirstWhitespace);
               //
-              place = moreInfo.substring(indexOfFirstWhitespace+1, moreInfo.length-1);
+              var place = moreInfo.substring(indexOfFirstWhitespace+1, moreInfo.length-1);
               //
-              teacher = curr.children[7].innerHTML;
+              var teacher = curr.children[7].innerHTML;
               //
               strToCopy += (title + "$" + course + "$" + courseType + "$" + teacher + "$" + day  + "$" + time + "$" + place + "\r\n");
           }
